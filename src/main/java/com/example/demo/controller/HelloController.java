6ec3e7e03a8e8dc5488e57f66ff4e21989f59cf5
adammeh <adammeh@kth.se>
2025-09-08 14:31:04 +0200

    test3

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -12,6 +12,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Newer Test from " + version;
+        return "Newest Test from " + version;
     }
 }
